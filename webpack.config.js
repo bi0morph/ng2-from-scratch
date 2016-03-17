686ec96fe84f54c629a70ce61af60920919d0cdc
Ruslan Ustyuhov <rusustyugov@gmail.com>
2016-03-17 02:31:18 +0200

    Add webpack.config.js file and some simpe config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./app/root.ts",
+  debug: true,
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+  output: {
+    path: "./build",
+    filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'awesome-type-script-loader', exclude: [ /\.(spec|e2e).ts$/] }
+    ]
+  }
+};
